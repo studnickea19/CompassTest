bc1897699c28231563a737bb7d6609a6e3c7cfba
studnickea19 <36928178+studnickea19@users.noreply.github.com>
2018-07-10 13:18:00 -0500

    Project details button is toggling, working on getting content to show

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,42 @@
 $(document).ready(function(){
 
-	$('project-details').hide();
+	$('.project-details').hide();
 
+	//Technical Skills Hover Effect
 	$('div.language-slider, img.details-button').mouseenter(function(){
 		$(this).css("background-color", "#CACFD2");
 	}).mouseleave(function(){
 		$(this).css("background-color", "#D7DBDD");
 	});
 
-	$('div.details-button').click(function(){
-		$(this).closest('div.project-details').toggle();
-	});
+	//Project Details Button Animation
+	$('.details-button').click(function() {	
+ 		$(this).closest('div.project-details').next().toggle();
+ 		$(this).closest('div.project').next().hide();
+ 	}, function() {
+ 		$('.arrow')
+ 		$('.project-details').toggle();
+  	});
+
+
+
+	//Div is hidden
+	//click button
+	//closest div (details) is toggled
+//closest div to details is hidden
+
+
+
+
+
+	// $('div.details-button').click(function(){
+	// 	$(this).closest('div.project-details').next().toggle();
+	// });
+
+	// // $('div.details-button').on('click', event => {
+ // //    $(event.currentTarget).closest('.product-details').next().toggle()
+ // //  }); 
+ 	
 
 
 
